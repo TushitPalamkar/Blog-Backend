ea397f2cb6d5b7961369356814d06a10af0cda64
TushitPalamkar <143091687+TushitPalamkar@users.noreply.github.com>
2024-07-28 15:22:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,7 @@ const app=express();
 const port=process.env.PORT || 4000;
 app.use(express.json())
 
-app.use(cors({
-    origin:'http://localhost:3000',
-    credentials:true
-}))
+app.use(cors())
 
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cookie())
